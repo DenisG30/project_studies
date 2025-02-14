111f9e326e4bcc4ba91cf1f0f38a6f44e8db8c46
DenisG30 <gorbunowdenis30@gmail.com>
2025-02-14 19:11:19 +0400

    Update ReadAndSumValueOfFiles.java

--- Сhanging/ReadAndSumValueOfFiles.java
+++ Сhanging/ReadAndSumValueOfFiles.java
@@ -3,72 +3,73 @@ import java.io.*;
 import java.util.Random;
 
 public class ReadAndSumValueOfFiles {
+    static final int ER_WRONG = 1;
+    static final int ER_READ_F = 2;
+    static final int ER_CLOSE_F = 3;
 
-    public static String SumValue(String num1, String num2) {
+    public static int SumValue(String num1, String num2) {
         int sum1 = 0;
-        BufferedReader brOne = null;
-        try{
-            File my_fileOne = new File(num1);
-            brOne = new BufferedReader(new FileReader(my_fileOne));
-            String st1;
-            int count1 = 0;
-            while ((st1 = brOne.readLine()) != null) {
-                count1 ++;
-                try {
-                    sum1 += Integer.parseInt(st1); 
-                } catch (NumberFormatException ex) {
-                    return "Ошибка! Строка " + count1 + " не является числом. " + ex.getMessage();
-                }   
-            }
-        } catch(IOException e) {
-            return "Ошибка при чтении файла: " + e.getMessage();
-        } finally {   
-            if (brOne != null) {
-                try {
-                    brOne.close();
-                } catch (IOException ee) {
-                    return "Ошибка при закрытии  файла: " + ee.getMessage();
-                }
-            }
-        }  
-
-        int sum2= 0;
-        BufferedReader brTwo = null;
-        try{
-            File my_fileTwo = new File(num2);
-            brTwo = new BufferedReader(new FileReader(my_fileTwo));
-            String st2;
-            int count2 = 0;
-            while ((st2 = brTwo.readLine()) != null) {
-                count2 ++;
-                try {
-                    sum2 += Integer.parseInt(st2); 
-                } catch (NumberFormatException ex) {
-                    return "Ошибка! Строка " + count2 + " не является числом. " + ex.getMessage();
-                }   
-            }
-        } catch(IOException e) {
-            return "Ошибка при чтении файла: " + e.getMessage();
-        } finally {   
-            if (brTwo != null) {
-                try {
-                    brTwo.close();
-                } catch (IOException ee) {
-                    return "Ошибка при закрытии  файла: " + ee.getMessage();
-                }
-            }
+        int res1 = readAndSum(num1, sum1);
+        if (res1 == ER_WRONG || res1 == ER_READ_F || res1 == ER_CLOSE_F) {
+            return res1;
         }
 
-        int sum = sum1 + sum2;
-        return String.valueOf(sum);
-    } 
+        int sum2 = 0;
+        int res2 = readAndSum(num2, sum2);
+        if (res2 == ER_WRONG || res2 == ER_READ_F || res2 == ER_CLOSE_F) {
+            return res2;
+        }
+        
+        int sum = res1 + res2;
+        return sum;
+    }
+    
+    private static int readAndSum(String num, int sum) {
+        BufferedReader br = null;
+        try{
+            File my_file = new File(num);
+            br = new BufferedReader(new FileReader(my_file));
+            String st;
+            while ((st = br.readLine()) != null) {
+                try {
+                    sum += Integer.parseInt(st);
+                } catch (NumberFormatException ex) {
+                    return ER_WRONG;
+                }   
+            }
+        } catch(IOException e) {
+            return ER_READ_F;
+        } finally {   
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException ee) {
+                    return ER_CLOSE_F;
+                }
+            }   
+        }
+        return sum; 
+    }
+    
     public static void main(String[] args) {
         Random rand = new Random();
-
         String fileWay1 = (rand.nextInt(9) + 1) + ".txt";
         String fileWay2 = (rand.nextInt(9) + 1) + ".txt";
+        int res = SumValue(fileWay1, fileWay2);
 
-        System.out.println(SumValue(fileWay1, fileWay2));
-        
+        switch (res) {
+            case ER_WRONG:
+                System.out.println("Ошибка! В одном из файлов присутствует недопустимое число.");
+                break;
+            case ER_READ_F:
+                System.out.println("Ошибка при чтении файла.");
+                break;
+            case ER_CLOSE_F:
+                System.out.println("Ошибка при закрытии файла.");
+                break;
+            default:
+                System.out.println("Сумма всех значений равна " + res);
+                break;
+        }   
     }
 }
