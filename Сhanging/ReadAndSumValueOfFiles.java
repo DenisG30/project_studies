e805528f4077279c8e39c4f5685460945580dd19
DenisG30 <gorbunowdenis30@gmail.com>
2025-02-18 03:30:41 +0400

    Update ReadAndSumValueOfFiles.java
    Added arrays to the solution

--- Сhanging/ReadAndSumValueOfFiles.java
+++ Сhanging/ReadAndSumValueOfFiles.java
@@ -3,72 +3,73 @@ import java.io.*;
 import java.util.Random;
 
 public class ReadAndSumValueOfFiles {
+    static final int OK = 0;
     static final int ER_WRONG = 1;
     static final int ER_READ_F = 2;
     static final int ER_CLOSE_F = 3;
 
-    public static int SumValue(String[] num) {
-        int sum = 0;
-        for(int i = 0; i < num.length; i++) {
-            String num1 = num[i];
-            int sum1 = 0;
-            int res1 = readAndSum(num1, sum1);
-            if (res1 == ER_WRONG || res1 == ER_READ_F || res1 == ER_CLOSE_F) {
-                return res1;
-            }
-            sum += res1;
+    public static int[] SumValue(String[] num) {
+        int[] res = new int[2]; // код ошибки и общая сумма
+        res[0] = 0;
+        res[1] = 0;
+        
+        for (int i = 0; i < num.length; i++) {
+            int[] arr = readAndSum(num[i]);
+            if (arr[0] != 0) {
+                res[0] = arr[0];
+                return res;
+            }    
+            res[1] += arr[1];
         }
-        /* 
-        int sum2 = 0;
-        int res2 = readAndSum(num2, sum2);
-        if (res2 == ER_WRONG || res2 == ER_READ_F || res2 == ER_CLOSE_F) {
-            return res2;
-        }
-        */
-
-        //int sum = res1 + res2;
-        return sum;
+        return res;
     }
     
-    private static int readAndSum(String num, int sum) {
+    private static int[] readAndSum(String num) {
+        int[] arr = new int[2];
+        int sum = 0;
+        arr[0] = 0; // Статус
+        arr[1] = 0;  // Сумма
         BufferedReader br = null;
+        
         try{
             File my_file = new File(num);
             br = new BufferedReader(new FileReader(my_file));
             String st;
-            while ((st = br.readLine()) != null) {
+            while ((st = br.readLine()) != null){
                 try {
                     sum += Integer.parseInt(st);
                 } catch (NumberFormatException ex) {
-                    return ER_WRONG;
+                    arr[0] = 1;
                 }   
             }
+            
         } catch(IOException e) {
-            return ER_READ_F;
+            arr[0] = 2;
         } finally {   
             if (br != null) {
                 try {
                     br.close();
                 } catch (IOException ee) {
-                    return ER_CLOSE_F;
+                    arr[0] = 3;
                 }
-            }   
+            }
         }
-        return sum; 
+        arr[1] = sum;
+        return arr; 
     }
     
     public static void main(String[] args) {
-        //Random rand = new Random();
-
         String[] fileWay1 = {"1.txt", "2.txt", "3.txt", "4.txt"};
-        //String fileWay1 = (rand.nextInt(9) + 1) + ".txt";
-        //String fileWay2 = (rand.nextInt(9) + 1) + ".txt";
+    
+        int[] res = new int[2];
+        res = SumValue(fileWay1);
 
-        int res = SumValue(fileWay1);
-
-        switch (res) {
+        switch (res[0]) {
+            case OK:
+                    System.out.println("Сложение завершено! Сумма значений равна " + res[1]);   
+                break;
             case ER_WRONG:
-                System.out.println("Ошибка! В одном из файлов присутствует недопустимое число.");
+                    System.out.println("Ошибка! В одном из файлов присутствует недопустимое число.");   
                 break;
             case ER_READ_F:
                 System.out.println("Ошибка при чтении файла.");
@@ -76,9 +77,6 @@ public class ReadAndSumValueOfFiles {
             case ER_CLOSE_F:
                 System.out.println("Ошибка при закрытии файла.");
                 break;
-            default:
-                System.out.println("Сумма всех значений равна " + res);
-                break;
-        }   
+        }
     }
 }
