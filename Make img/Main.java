5e41cdc6bb58111605311bd683a065b5226749e0
DenisG30 <gorbunowdenis30@gmail.com>
2025-02-22 15:02:00 +0400

    Update Main.java

--- Make img/Main.java
+++ Make img/Main.java
@@ -1,6 +1,24 @@
 
 public class Main{
     public static void main(String[] args) {
-        MakeImg.makeImg("test", "jpg", "png");
+       HashMap<String, Integer> res = new HashMap<String, Integer>();
+        res = MakeImg.makeImg("test", "jpg", "png");
+        System.out.println("");
+        for (String key : res.keySet()) {
+            switch (res.get(key)) {
+                case 0:
+                        System.out.println("Создание из изображения: " + key + "– успешно завершено!");
+                    break;
+                case 1:
+                        System.out.println("Директория не найдена или не является директорией.");   
+                    break;
+                case 2:
+                    System.out.println("Не удалось прочитать изображение: " + key);
+                    break;
+                case 3:
+                    System.out.println("Ошибка при обработке файла: " + key);
+                    break;
+            }
+        }
     }
 }
