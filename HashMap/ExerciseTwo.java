7a2c9960b05cf97cbcd45cc90ed7d9d0b38b756f
DenisG30 <gorbunowdenis30@gmail.com>
2025-02-22 16:32:17 +0400

    Update ExerciseTwo.java

--- HashMap/ExerciseTwo.java
+++ HashMap/ExerciseTwo.java
@@ -4,19 +4,16 @@ import java.util.HashMap;
 
 public class ExerciseTwo {
     public static ArrayList<Integer> keyValue(ArrayList<Integer> mylist, int n) {
-
         HashMap<Integer, Integer> map = new HashMap<>();
+        ArrayList<Integer> num = new ArrayList<>();
 
         for (int number : mylist) {
             map.put(number, map.getOrDefault(number, 0) + 1);
-        }
-
-         ArrayList<Integer> num = new ArrayList<>();
-        
-        for (HashMap.Entry<Integer, Integer> meaning : map.entrySet()) {
-             if (meaning.getValue() >= n) {
-                 num.add(meaning.getKey());
-             }
+            if (map.get(number) == n) { 
+                num.add(number);
+            } else if (!num.contains(number) && map.get(number) > n) { 
+                num.add(number);
+            }
         }
         return num;
     }
